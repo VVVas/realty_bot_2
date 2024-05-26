4130dcaa5f834e2da3adff622fa68e873903ad31
Evgeny Turubanov <eturubanov@yandex.ru>
2024-05-26 19:42:47 +0300

    Add User and Profile model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from users.models import Profile, User
+
+admin.site.empty_value_display = '_пусто_'
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'id', 'username', 'email', 'first_name', 'last_name', 'is_active',
+        'is_staff'
+    )
+    list_filter = ('username', 'email')
+    list_editable = ('is_active', 'is_staff')
+    search_fields = ('email', 'username')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'external_id', 'username', 'first_name', 'last_name', 'is_active'
+    )
+
+
+admin.site.unregister(Group)
