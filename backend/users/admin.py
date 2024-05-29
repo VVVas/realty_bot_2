eb4b2a4b5d770605bc81b7c96ad5208e2bd0ef15
Nikita Peschanov <keyngr@gmail.com>
2024-05-29 14:21:04 +0400

    fix admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.contrib.admin import display
-from django.utils.safestring import mark_safe
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
-from users.models import Profile, User
-from realties.models import Ad, Category, City, Comment, Photo, Realty
+from .models import Profile, User
+
 
 admin.site.empty_value_display = '_пусто_'
 admin.site.unregister(Group)
@@ -40,61 +39,3 @@ class ProfileAdmin(admin.ModelAdmin):
     @display(description='Комментарии')
     def comments_count(self, user):
         return user.comment.filter(user=user).count()
-
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-    search_fields = ('title',)
-
-
-@admin.register(City)
-class CityAdmin(admin.ModelAdmin):
-    list_display = ('title', 'timezone',)
-    search_fields = ('title',)
-
-@admin.register(Realty)
-class RealtyAdmin(admin.ModelAdmin):
-    list_display = (
-        'title', 'city', 'address', 'category', 'image_preview',
-    )
-    list_filter = ('city', 'category',)
-    search_fields = ('title', 'city', 'address')
-
-    @mark_safe
-    @display(description='Фото')
-    def image_preview(self, realty):
-        return (
-            f'<img src="{realty.img.url}" style="max-height: 70px;">'
-        )
-
-
-@admin.register(Ad)
-class AdAdmin(admin.ModelAdmin):
-    list_display = (
-        'title', 'realty', 'address', 'is_published',
-        'added_in_favorites'
-    )
-
-    @display(description='В избранных')
-    def added_in_favorites(self, ad):
-        return ad.favorite_users.count()
-
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'ad', 'text', 'date_create', 'is_validate',)
-    list_filter = ('is_validate',)
-
-
-@admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('user', 'ad', 'date', 'is_validate', 'image_preview',)
-    list_filter = ('is_validate',)
-
-    @mark_safe
-    @display(description='Фото')
-    def image_preview(self, photo):
-        return (
-            f'<img src="{photo.img.url}" style="max-height: 70px;">'
-        )
