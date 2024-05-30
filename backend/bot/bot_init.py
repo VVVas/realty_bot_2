74210f8a91a1e0d675ea01412b2866d494953c39
Сулайманова Сайкал <sulaymanovasaykal2003gmail.com>
2024-05-30 20:45:09 +0600

    v 21.2

--- backend/bot/bot_init.py
+++ backend/bot/bot_init.py
@@ -1,22 +1,17 @@
 from django.conf import settings
-from telegram import Bot
-from telegram.ext import Dispatcher
+from telegram.ext import Application
 
-from .handlers import handler
-
-
-def telegram_bot():
-    bot = Bot(settings.TOKEN_BOT)
-    return bot
+from handlers import handler
 
 
-def telegram_dispatcher():
-    bot = telegram_bot()
-    dispatcher = Dispatcher(bot, None, workers=0)
-    dispatcher.add_handler(handler)
-
-    return dispatcher
+def telegram_application():
+    application = Application.builder().token(settings.TOKEN_BOT).build()
+    application.add_handler(handler)
+    return application
 
 
-BOT = telegram_bot()
-DISPATCHER = telegram_dispatcher()
+APPLICATION = telegram_application()
+
+
+APPLICATION.updater.start_polling()
+APPLICATION.updater.idle()
