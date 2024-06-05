0560ac5f41b553a12b0e369ebc400ccf1e8a0179
Nikita Peschanov <keyngr@gmail.com>
2024-06-05 16:09:49 +0400

    admin images

--- backend/bot/handlers.py
+++ backend/bot/handlers.py
@@ -1,56 +1,56 @@
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    filters,
-    ConversationHandler,
-    CallbackContext,
-)
+# from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+# from telegram.ext import (
+#     Updater,
+#     CommandHandler,
+#     MessageHandler,
+#     filters,
+#     ConversationHandler,
+#     CallbackContext,
+# )
 
-from .models import BotMessage
+# from .models import BotMessage
 
-HELLO, INFO, WORK = range(3)
-
-
-async def start(update: Update, _: CallbackContext):
-    reply_keyboard = [['Информация о боте', 'Начать поиск недвижимости']]
-    await update.message.reply_text(
-        Message.objects.filter(keyword='START')[0].text,
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True
-        )
-    )
-
-    return HELLO
+# HELLO, INFO, WORK = range(3)
 
 
-async def info(update: Update, _: CallbackContext):
-    await update.message.reply_text(
-        Message.objects.filter(keyword='HELP')[0].text
-    )
+# async def start(update: Update, _: CallbackContext):
+#     reply_keyboard = [['Информация о боте', 'Начать поиск недвижимости']]
+#     await update.message.reply_text(
+#         Message.objects.filter(keyword='START')[0].text,
+#         reply_markup=ReplyKeyboardMarkup(
+#             reply_keyboard, one_time_keyboard=True
+#         )
+#     )
 
-    return ConversationHandler.END
-
-
-async def work(update: Update, _: CallbackContext):
-    await update.message.reply_text(
-        'Здесь будет логика работы бота, но позже'
-    )
+#     return HELLO
 
 
-async def cancel(update: Update, _: CallbackContext):
-    await update.message.reply_text(
-        Message.objects.filter(keyword='CANCEL')[0].text
-    )
-    return ConversationHandler.END
+# async def info(update: Update, _: CallbackContext):
+#     await update.message.reply_text(
+#         Message.objects.filter(keyword='HELP')[0].text
+#     )
+
+#     return ConversationHandler.END
 
 
-handler = ConversationHandler(
-    entry_points=[CommandHandler('start', start)],
-    states={
-        INFO: [CommandHandler('info', info)],
-        WORK: [CommandHandler('work', work)]
-    },
-    fallbacks=[CommandHandler('cancel', cancel)]
-)
+# async def work(update: Update, _: CallbackContext):
+#     await update.message.reply_text(
+#         'Здесь будет логика работы бота, но позже'
+#     )
+
+
+# async def cancel(update: Update, _: CallbackContext):
+#     await update.message.reply_text(
+#         Message.objects.filter(keyword='CANCEL')[0].text
+#     )
+#     return ConversationHandler.END
+
+
+# handler = ConversationHandler(
+#     entry_points=[CommandHandler('start', start)],
+#     states={
+#         INFO: [CommandHandler('info', info)],
+#         WORK: [CommandHandler('work', work)]
+#     },
+#     fallbacks=[CommandHandler('cancel', cancel)]
+# )
--- backend/bot/views.py
+++ backend/bot/views.py
@@ -1,23 +1,23 @@
-from handlers2 import *
-from django.conf import settings
+# from handlers2 import *
+# from django.conf import settings
 
-TOKEN = settings.TOKEN_BOT
+# TOKEN = settings.TOKEN_BOT
 
 
-class TelegramBotView(View):
-    bot = Bot(token=TOKEN)
+# class TelegramBotView(View):
+#     bot = Bot(token=TOKEN)
 
-    def post(self, request, *args, **kwargs):
-        update = Update.de_json(json.loads(request.body), self.bot)
-        application = Application.builder().token(TOKEN).build()
-        application.add_handler(CommandHandler("start", start))
-        application.add_handler(CommandHandler("ads", ads))
-        application.add_handler(CommandHandler("filter_ad_category", filter_ad_category))
-        application.add_handler(conv_handler)
+#     def post(self, request, *args, **kwargs):
+#         update = Update.de_json(json.loads(request.body), self.bot)
+#         application = Application.builder().token(TOKEN).build()
+#         application.add_handler(CommandHandler("start", start))
+#         application.add_handler(CommandHandler("ads", ads))
+#         application.add_handler(CommandHandler("filter_ad_category", filter_ad_category))
+#         application.add_handler(conv_handler)
 
-        async def process_update():
-            await application.process_update(update)
+#         async def process_update():
+#             await application.process_update(update)
 
-        application.create_task(process_update())
+#         application.create_task(process_update())
 
-        return JsonResponse({"status": "ok"})
+#         return JsonResponse({"status": "ok"})
