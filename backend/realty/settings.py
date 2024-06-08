6438ced65b59bbef4206ce5827ec00d35a305ee9
Nikita Peschanov <stas.basov@whatever.com>
2024-06-08 19:43:35 +0400

    admin import

--- backend/realty/settings.py
+++ backend/realty/settings.py
@@ -1,13 +1,13 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env_file_path = '../infra/.env'
-load_dotenv(dotenv_path=env_file_path)
+# env_file_path = '../infra/.env'
+# load_dotenv(dotenv_path=env_file_path)
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', '-1')
 GENERAL_URL = os.getenv('GENERAL_URL', default='https://mysite.com/')
 # Quick-start development settings - unsuitable for production
