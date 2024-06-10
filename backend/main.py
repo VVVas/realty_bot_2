e4d4a2b8205736a7fc5e408835ac688f003bfffc
VVVas <v@vvvas.ru>
2024-06-10 19:15:08 +0300

    localhost

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,7 @@ async def main():
             app=get_asgi_application(),
             port=PORT,
             use_colors=True,
-            host='0.0.0.0',
+            host='localhost',
             log_level=logging.DEBUG,
         )
     )
