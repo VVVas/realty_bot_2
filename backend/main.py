a8ce34733434e0b165cb605e393c6b91e556bccd
VVVas <v@vvvas.ru>
2024-06-11 17:33:02 +0300

    DJANGO_ALLOW_ASYNC_UNSAFE

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 import asyncio
-# import json
 import logging
 import os
 
@@ -7,28 +6,15 @@ import django
 import uvicorn
 from django.conf import settings
 from django.core.asgi import get_asgi_application
-# from django.http import HttpRequest, HttpResponse
-# from django.views.decorators.csrf import csrf_exempt
 from telegram import Update
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realty.settings')
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
 
 from bot import bot_init  # noqa E402
 
-
 URL = settings.GENERAL_URL  # Set URL for WebHook
-
-
-# @csrf_exempt
-# async def webhook(request: HttpRequest) -> HttpResponse:
-#     """Обрабатываем полученное от бота сообщение."""
-#     await tgbot_core.tgbot.ptb_app.update_queue.put(
-#         Update.de_json(
-#             data=json.loads(request.body),
-#             bot=tgbot_core.tgbot.ptb_app.bot)
-#     )
-#     return HttpResponse()
 
 
 async def main():
