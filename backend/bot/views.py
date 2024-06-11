33b054e15a97776e09a152413709f5dae29694c5
VVVas <v@vvvas.ru>
2024-06-11 18:56:38 +0300

    Удалил лишнее из бота

--- backend/bot/views.py
+++ backend/bot/views.py
@@ -1,31 +1,10 @@
 import json
-# import logging
 
-# from django.http import JsonResponse
-# from django.views import View
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-# from django.utils.decorators import method_decorator
 from telegram import Update
 
 from . import bot_init
-
-
-# logger = logging.getLogger(__name__)
-
-
-# @method_decorator(csrf_exempt, name='dispatch')
-# class TelegramBotView(View):
-
-#     def post(self, request, *args, **kwargs):
-#         update = Update.de_json(json.loads(request.body), tgbot.ptb_app)
-
-#         async def process_update():
-#             await tgbot.ptb_app.process_update(update)
-
-#         tgbot.ptb_app.create_task(process_update())
-
-#         return JsonResponse({"status": "ok"})
 
 
 @csrf_exempt
