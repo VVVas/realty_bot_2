f65f2a2e744b0c0925a50d1f51cc3b7834935f3f
VVVas <v@vvvas.ru>
2024-06-11 17:07:38 +0300

    наш бот

--- backend/realty/urls.py
+++ backend/realty/urls.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.urls import path
 
 # from bot.views import TelegramBotView
-from main import webhook
+from bot.views import webhook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('webhook/', TelegramBotView.as_view(), name='webhook')
     path('webhook/', webhook, name='webhook')
 ]
