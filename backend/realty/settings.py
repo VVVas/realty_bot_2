66d4048c653953c7884b2fe9a024aba6551fa217
Nikita Peschanov <stas.basov@whatever.com>
2024-06-12 20:26:48 +0400

    fix botmessage import

--- backend/realty/settings.py
+++ backend/realty/settings.py
@@ -30,13 +30,13 @@ CSRF_TRUSTED_ORIGINS = ['https://pb.vvvas.ru']
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_reorder',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_reorder',
     'rangefilter',
     'import_export',
     'bot.apps.BotConfig',
@@ -150,7 +150,7 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 ADMIN_REORDER = (
     {
         'app': 'realties',
-        'label': 'Недвижимость и объявления',
+        'label': 'Недвижимость и объявления ',
         'models': (
             'realties.Realty',
             'realties.Ad',
@@ -177,6 +177,14 @@ ADMIN_REORDER = (
         )
     },
 )
+
+# JAZZMIN_SETTINGS = {
+#     "show_sidebar": False,
+#     "topmenu_links": [
+#         {"name": "Главная",  "url": "admin:index", "permissions": ["auth.view_user"]},
+#     ],
+# }
+# JAZZMIN_SETTINGS["show_ui_builder"] = True
 
 ADMIN_PERMISSIONS = [
     'change_profile',
--- backend/realty/urls.py
+++ backend/realty/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 # from bot.views import TelegramBotView
 from bot.views import webhook
 
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webhook/', webhook, name='webhook')
