08eadf841a9f595180ff606a36adfc0a30e6dbfb
Максим Кавтырев <kavtyrev@vk.com>
2024-06-14 14:35:08 +0300

    fix dialog

--- backend/bot/handlers2.py
+++ backend/bot/handlers2.py
@@ -16,6 +16,8 @@ COMMENT, ADD_COMMENT, COMMENT_INPUT = range(8, 11)
 
 async def start(update: Update, context: CallbackContext) -> int:
     greeting_message = get_botmessage_by_keyword('WELCOME')
+    if context.user_data.get('START_OVER'):
+        greeting_message = 'Выберите нужное действие'
     Profile.objects.get_or_create(
         external_id=update.message.from_user.id,
         username=update.message.from_user.username,
@@ -183,7 +185,7 @@ async def select_price(update: Update, context: CallbackContext) -> int:
 
     context.user_data.clear()
 
-    return ConversationHandler.END
+    return await cancel(update, context)
 
 
 async def comment(update: Update, context: CallbackContext):
@@ -282,6 +284,8 @@ async def favorite(update: Update, context: CallbackContext):
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
 
+    return await cancel(update, context)
+
 
 async def delete_favorite(update: Update, context: CallbackContext):
     query_data = update.callback_query.data.split(',')
@@ -311,10 +315,11 @@ async def delete_user(update: Update, context: CallbackContext):
     Profile.objects.get(external_id=update.message.from_user.id).delete()
     delete_profile_message = get_botmessage_by_keyword('DELETE_PROFILE')
     await update.message.reply_text(delete_profile_message)
+    return await cancel(update, context)
 
 
 async def cancel(update: Update, context: CallbackContext) -> int:
-    context.user_data.clear()
+    context.user_data['START_OVER'] = True
     return await start(update, context)
 
 
