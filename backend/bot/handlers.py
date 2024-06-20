6295701614e741e25cc36ea45e6dc5b7b61b44da
VVVas <v@vvvas.ru>
2024-06-20 19:01:43 +0300

    кавычки

--- backend/bot/handlers.py
+++ backend/bot/handlers.py
@@ -373,7 +373,7 @@ async def cancel(update: Update, context: CallbackContext) -> int:
 
 
 search_conv_handler = ConversationHandler(
-    entry_points=[CommandHandler("start", start)],
+    entry_points=[CommandHandler('start', start)],
     states={
         START: [
             MessageHandler(filters.Regex('^(О боте)$'), help_command),
